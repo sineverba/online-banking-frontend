29cbe8af25f79c7b07e83817a8ff559eaa47286d
sineverba <alessandro.arici@gmail.com>
2023-01-11 16:22:13 +0100

    Remove token middleware on logout

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 TODO
 ====
 
-+ Remove all localstorage
 + Merge all splices (https://redux-toolkit.js.org/rtk-query/usage/code-splitting)
 + Invalidate the tags (https://redux-toolkit.js.org/rtk-query/api/created-api/api-slice-utils#invalidatetags)
-+ Add all locastorage (login and logout)
++ Add Footer (version) to home page
---
+++ src/middlewares/removeAccessTokenMiddleware.js
@@ -0,0 +1,19 @@
+import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { LOCALSTORAGE_ACCESS_TOKEN } from "../utils/constants/constant";
+
+/**
+ * When we receive action to delete loginSlice,
+ * delete localStorage
+ */
+
+const type = `loginSlice/resetApiState`;
+
+const removeAccessTokenMiddleware = createListenerMiddleware();
+removeAccessTokenMiddleware.startListening({
+  type,
+  effect: () => {
+    localStorage.removeItem(LOCALSTORAGE_ACCESS_TOKEN);
+  }
+});
+
+export default removeAccessTokenMiddleware;
